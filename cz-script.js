6a04d96ed00a6cb00b1d90cb75d8ec8ebe0692c0
Cynthia Zhong <cynthia.zz@berkeley.edu>
2019-12-28 14:49:11 +0800

    js stuff done. need to finish skills

---
+++ cz-script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	
+	var headerHeight = $('#header').height()
+
+	// highlight link corresponding to section on screen
+	// when user scrolls
+	$(window).scroll(function(){
+		// scroll pos of current window
+		var currScroll = $(document).scrollTop();
+
+		var currSection;
+
+		$('.section').each(function(){
+			// this section's offset from top
+			var secScroll = $(this).offset().top;
+
+			// if we've scrolled past this section, set it to curr
+			if (secScroll - headerHeight - 1 <= currScroll){
+				currSection = $(this);
+			}
+		})
+
+		// remove active tag
+		$('.section-link').removeClass('active');
+
+		// highlight active section
+		var id = currSection.attr('id');
+		$('a[href="#'+id+'"]').addClass('active');
+	})
+
+	// slowly scroll to clicked section
+	$('.section-link').click(function(){
+		// id of clicked section
+		var id = $(this).attr('href');
+
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - headerHeight
+		}, 800);
+	})
+})
